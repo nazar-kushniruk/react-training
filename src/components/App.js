895ba88c08f2cbd680be2fa8f6a4478c5ed384d0
nazar <kushniruknazar@equalteam.org.ua>
2019-03-27 17:50:11 +0200

    add styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import articles from './fixtures'
-import Article from './Article'
+import ArticleList from './ArticleList'
+import 'bootstrap/dist/css/bootstrap.css'
 
-function App() {
-    console.log(articles);
-    return (
-        /*<div> {articles[0].title}</div>*/
-        <Article article={articles[0]}></Article>
-    )
+class App extends Component {
+    /*constructor(props){
+        super(props)
+        this.state = {
+            data: null
+        }
+    }*/
+
+    /* componentWillMount() {
+         this.setState({
+             data :  articles
+         })
+ }*/
+    state = {
+        reverted: false
+    }
+
+    render() {
+        const data = articles;
+        return (
+            <div className='container'>
+                <div className='jumbotron'>
+                    <h1 className='display-3'>My App</h1>
+                    <button className='btn btn-primary btn-lg' onClick={this.revert}>Revert</button>
+                </div>
+
+                <ArticleList articles={this.state.reverted ? data.reverse() : data } />
+            </div>
+        )
+
+    }
+    revert = () => {
+        this.setState({
+            reverted:!this.state.reverted
+        })
+        console.log('this.state.reverted->',this.state.reverted)
+    }
 }
 
+
 export default App
+
+
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,15 +1,87 @@
-import React from 'react';
+import React, {PureComponent} from 'react';
 
-function Article(props) {
-const {article} = props;
-console.log('from article ', article);
-    return (
-        <div>
-        <h1> {article.title}</h1>
-            <p>{article.text}</p>
-            <hr/>
-        </div>
-    )
+class Article extends PureComponent {
+
+    constructor(props) {
+            super(props)
+           // console.log('props.defaultOpen',props.defaultOpen)
+            this.state = {
+                isOpen: props.defaultOpen,
+                clicked: 0
+            }
+        }
+
+   /* shouldComponentUpdate(nextProps,nextState){
+
+        return nextState.isOpen !== this.state.isOpen
+    }
+*/
+    componentWillMount() {
+        console.log('---', 'mounting');
+    }
+
+    componentWillReceiveProps(nextProps){
+        if(nextProps.defaultOpen !== this.props.defaultOpen) {
+            this.setState({
+                isOpen : nextProps.defaultOpen
+            })
+        }
+       // console.log('---', 'componentWillReceiveProps');
+    }
+
+    componentWillUpdate() {
+        console.log('---', 'componentWillUpdate');
+
+    }
+
+    //handleClick = handleClick.bind(this);
+    render() {
+        const {article} = this.props;
+        //debugger;
+
+        const body = this.state.isOpen && <section className='card-text'>{article.text}</section>
+        return (
+            <div className='card mx-auto' style={{width : '50%'}}>
+                <div className='card-header'>
+                    <h1 onClick={this.clickCounter}>{article.title}{this.state.clicked}
+                        <button onClick={this.handleClick} className='btn btn-primary btn-lg float-right'>{this.state.isOpen ? 'CLOSE' : 'OPEN'}</button>
+                    </h1>
+                </div>
+               <div className='card-body'>
+                  <h6 className='card-subtitle text-muted'>Creation date: { (new Date(article.date)).toDateString()}</h6>
+                   {body}
+               </div>
+
+            </div>
+        )
+
+    }
+
+    handleClick = () => {
+        this.setState({isOpen: !this.state.isOpen})
+
+    }
+    clickCounter = () => {
+        this.setState({clicked : this.state.clicked + 1})
+    }
 }
 
-export default Article;
+export default Article
+
+/* {article && article.map((item) => (
+                    <React.Fragment>
+                        <li>{item.title}
+                        <button onClick={this.handleClick} value={item.title} >Nazar</button>
+                        </li>
+                        <li>{item.text}</li>
+                        <li>{item.date}</li>
+                        <hr/>
+                        <br/>
+                    </React.Fragment>
+
+                ))}
+                {!article &&
+                        <div>
+                            items not found
+                        </div>
+            }*/
